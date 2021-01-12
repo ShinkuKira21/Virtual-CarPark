7235c4fa2412f54dc6698875cfa38a3a5f522282
ShinkuKira21 <crazygaming055@gmail.com>
2021-01-12 12:33:52 +0000

    Added Barrier Logic

--- C++/Parking Methods/Barrier.cpp
+++ C++/Parking Methods/Barrier.cpp
@@ -1,11 +1,18 @@
 #include "Barrier.h"
 
-Barrier::Barrier(CarPark& carParkObj) : CarPark(carParkObj)
+Barrier::Barrier(CarPark& carParkObj, bool state) : CarPark(carParkObj)
 {
-
+	SetState(state);
 }
 
-Barrier::~Barrier()
-{
+Barrier::~Barrier() { }
 
+void Barrier::SetState(bool state) { bBarrierStatus = state; }
+
+void Barrier::DisplayState() 
+{ 
+	if (bBarrierStatus)
+		std::cout << "Barrier is open!\n";
+	else
+		std::cout << "Barrier is closed!\n";
 }
